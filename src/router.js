ef99eecac4db5fa0afac4459ebaac01b4dbd4219
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-01-28 10:57:58 +0100

    hai

--- src/router.js
+++ src/router.js
@@ -1,24 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import AboutView from './views/AboutView.vue'
-import HomeView from './views/HomeView.vue'
-import HelloWorld from '../src/components/HelloWorld.vue'
-
+import AboutView from './views/AboutView.vue';
+import HomeView from './views/HomeView.vue';
+import HelloWorld from '../src/components/HelloWorld.vue';
+import Hai from './views/AboutHai.vue';
 
 export default createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      component: AboutView,
-      path: '/about/:'
-    },
-    {
-        component: HomeView,
-        path: '/'
-      },
-      {
-        component: HelloWorld,
-        path: '/hej'
-      }
-  ]
-})
+    history: createWebHistory(),
+    routes: [
+        {
+            component: AboutView,
+            path: '/about/:'
+        },
+        {
+            component: HomeView,
+            path: '/'
+        },
+        {
+            component: HelloWorld,
+            path: '/hej'
+        },
+        {
+            component: Hai,
+            path: '/about/Hai'
+        }
+    ]
+});
